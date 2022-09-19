4c84d2d36c005a9c025dd95e7f4f182a203cdc32
DetEMagic <anton.marhold@hotmail.com>
2022-09-20 01:25:29 +0200

    game_state maybe finished???

--- GameState.py
+++ GameState.py
@@ -8,41 +8,112 @@ class Position:
         self.z = z
 
 class Piece:
-    def __init__(self, position : Position , direction, color):
+    def __init__(self, position : Position , orientation : Orientation, color : Color):
         self.position = position
-        self.direction = direction
+        self.orientation = orientation
         self.color = color
     
     def get_position(self):
         return self.position
     
-    def set_position(self,x,y,z):
-        self.position.x = x
-        self.position.y = y
-        self.position.z = z
-    
-    def get_direction(self):
-        return self.direction
+    def get_orientation(self):
+        return self.orientation
 
-    def set_direction(self, direction):
-        self.direction = direction
-    
     def get_color(self):
         return self.color
+    
+    def __repr__(self):
+        return self.color.name
+
+    def __str__(self):
+        return self.color.name
 
     
 class GameState:
     def __init__(self, 
                 piecesPlayer1 = 21, 
                 piecesPlayer2 = 21, 
-                board = np.zeros(shape=(5,5,42))):
+                board = np.zeros(shape=(5,5,42), dtype=object)):
         self.piecesPlayer1 = piecesPlayer1
         self.piecesPlayer2 = piecesPlayer2
         self.board = board
     
-    def printState(self):
+    def print_state(self):
         print(self.board)
     
+    def update(self, move):
+        src_x = move["src"]["pos_x"]
+        src_y = move["src"]["pos_y"]
+
+        des_x = move["des"]["pos_x"]
+        des_y = move["des"]["pos_y"]
+
+        color = move["color"]
+        ori = move["des"]["orientation"]
+
+        des_z = self.board[des_x][des_y]
+
+        if move["first_turn"]:
+            if Color.WHITE.value == color.value: 
+                opponent_color = Color.BLACK
+            else:
+                opponent_color = Color.WHITE
+
+            for i in range(0, des_z.size):
+                if des_z[i] == 0:
+                    des_z[i] = Piece(Position(des_x, des_y, i), ori, opponent_color)
+                    break
+
+        elif move["src"]["pile"]:
+            for i in range(0, des_z.size):
+                if des_z[i] == 0:
+                    des_z[i] = Piece(Position(des_x, des_y, i), ori, color)
+                    break
+        else:
+            ###Add pieces to new destination
+            pieces = move["pieces"]
+
+            for i in range(0, des_z.size):
+                if des_z[i] == 0:
+                    for j in range(0, pieces-1):
+                        #All the pieces beneath the one on the top are flat
+                        des_z[j+i] = Piece(Position(des_x, des_y, j+i), Orientation.FLAT, color)
+                    
+                    des_z[j+pieces] = Piece(Position(des_x, des_y, j+i), ori, color)
+                    break
+            
+            ###Remove where the pieces were 
+            src_z = self.board[src_x, src_y]
+
+            for i in range(0, src_z.size):
+                if src_z[i] == 0:
+                    for j in range(0, pieces):
+                        src_z[i-j-1] = 0
+                    break
+
+move = {
+    "src": {
+        "pile": True,
+        "pos_x": 1,
+        "pos_y": 1,
+    },
+    "des": {
+        "pos_x": 0,
+        "pos_y": 0,
+        "orientation": Orientation.FLAT
+    },
+    "pieces": 1,
+    "color": Color.WHITE,
+    "first_turn": False
+}
 
 
-GameState().printState()
+
+game = GameState()
+game.update(move)
+game.update(move)
+game.print_state()
+
+
+move["des"]["pos_x"] = 0
+move["des"]["pos_y"] = 0
--- README.md
+++ README.md
@@ -2,29 +2,68 @@
 You Stack You Lose, The project for Group G 2022
 
 # Format of the board
-
 ```
-[ [[], [], [], [], []],
-  [[], [], [], [], []],
-  [[], [], [], [], []],
-  [[], [], [], [], []],
-  [[], [], [], [], []] ]
+[[[0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]]
+
+ [[0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]]
+
+ [[0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]]
+
+ [[0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]]
+
+ [[0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]
+  [0 0 0 ... 0 0 0]]]
 
 shape(x = 5, y = 5, z = 42)
 
 The maximum pieces in a stack is 42
-
 ```
-
 # Format of a move 
 
-* First turn 
 * Source
-    * Unused pieces
-    * Board
-        * Position
-        * The amount of pieces
+    * Is it from the Pile
+    * Position
 * Destination
-    * Postion
-    * Orientation
-        * If it is possible
+    * Position
+    * Orientation of the piece at the top
+* The amount of pieces
+* Color of the player
+* Is it the first turn in the game
+
+### Example of a move
+```
+move = {
+    "src": {
+        "pile": True,
+        "pos_x": 0,
+        "pos_y": 0,
+    },
+    "des": {
+        "pos_x": 0,
+        "pos_y": 0,
+        "orientation": Orientation.FLAT
+    },
+    "pieces": 1,
+    "color": Color.WHITE,
+    "first_turn": False
+}
+```
