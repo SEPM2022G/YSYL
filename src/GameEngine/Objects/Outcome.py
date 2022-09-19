6a9ee10e000b970997563eb187461206f2a34f1e
cjayasa <113280294+cjayasa@users.noreply.github.com>
2022-09-19 15:43:21 +0200

    Fixtypo

--- src/GameEngine/Objects/Outcome.py
+++ src/GameEngine/Objects/Outcome.py
@@ -1,5 +1,5 @@
 # This class represents a 'Outcome' object that can be passed between validator, controller and state manager components
 
-class State:
+class Outcome:
     def __init__(self) -> None:
         pass
