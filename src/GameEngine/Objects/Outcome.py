3bf6b731d830402732f25cdfc6cccb665b307a99
Carl Willman <carl@willman.nu>
2022-09-20 20:54:06 +0200

    Outcome Enum

--- src/GameEngine/Objects/Outcome.py
+++ src/GameEngine/Objects/Outcome.py
@@ -1,5 +1,11 @@
-# This class represents a 'Outcome' object that can be passed between validator, controller and state manager components
+from enum import Enum
 
-class Outcome:
-    def __init__(self) -> None:
-        pass
+class Outcome(Enum):
+    '''
+    represents a 'Outcome' object that can be passed between validator,
+    controller and state manager components
+    '''
+    INVALID = 0
+    VALID = 1
+    WIN_PLAYER = 2
+    WIN_AI = 3
