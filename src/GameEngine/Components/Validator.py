f687666792b7b2613af1dbdf356ad8ca62b2249e
Carl Willman <carl@willman.nu>
2022-09-22 18:58:13 +0200

    Move Validation

--- src/GameEngine/Components/Validator.py
+++ src/GameEngine/Components/Validator.py
@@ -1,43 +1,112 @@
 import numpy as np
 from src.GameEngine.Objects.Enums import Orientation, Color
 from src.GameEngine.Objects.Outcome import Outcome
-outcome = [ Outcome.WIN_WHITE, Outcome.WIN_BLACK ]
+outcome = [Outcome.WIN_WHITE, Outcome.WIN_BLACK]
+
 
 class Validator:
     '''
     Validate moves and checks for win condition.
     '''
 
-    def check(self, move, state) -> Outcome:
+    def check(self, move, oldstate, newstate) -> Outcome:
+        newboard = newstate["board"]
+        oldboard = oldstate["board"]
+
+        des = move["des"]
+        src = move["src"]
+
+        win_state = self._win_check(newboard)
+        if win_state != Outcome.CONT:
+            return win_state
+
+        if move["pieces"] < 1:
+            return Outcome.INVALID
+
+        # First round
+        if move["first_turn"] == True:
+            if src["pile"]:
+                return Outcome.INVALID
+            if move["pieces"] != 1:
+                return Outcome.INVALID
+
+        # des
+        if des["pos_x"] < 1 or des["pos_x"] > 5:
+            return Outcome.INVALID
+
+        if des["pos_y"] < 1 or des["pos_y"] > 5:
+            return Outcome.INVALID
+
+        des_elem = self._find_top(oldboard[des["pos_x"]][des["pos_y"]])
+        if des_elem != -1 and des_elem.get_color() != move["color"] and not move["first_turn"]:
+            return Outcome.INVALID
+
+        if des_elem != -1 and des_elem.get_orientation() == Orientation.STANDING:
+            print(des_elem)
+            return Outcome.INVALID
+
+        # src
+        if (src["pos_x"] < 1 or src["pos_x"] > 5) and not src["pile"]:
+            return Outcome.INVALID
+
+        if (src["pos_y"] < 1 or src["pos_y"] > 5) and not src["pile"]:
+            return Outcome.INVALID
+
+        src_elem = self._find_top(oldboard[src["pos_x"]][src["pos_y"]])
+        if not src["pile"] and src_elem.get_color() != move["color"]:
+            return Outcome.INVALID
+
+        if src["pile"] == True and not move["first_turn"]:
+            if move["color"] == Color.WHITE and oldstate["white_pieces_pile"] - move["pieces"] < 0:
+                return Outcome.INVALID
+            if move["color"] == Color.BLACK and oldstate["black_pieces_pile"] - move["pieces"] < 0:
+                return Outcome.INVALID
+
+        # Check that n pieces are correct color
+        src_stack = oldboard[src["pos_x"]][src["pos_y"]]
+        src_stack = src_stack[src_stack != 0]
+
+        for i, elem in enumerate(src_stack[-move["pieces"]:]):
+            src_stack[i] = elem.get_color() == move["color"]
+
+        if not src["pile"] and not np.all(src_stack):
+            return Outcome.INVALID
+
+        # Prevents idle move
+        if src["pos_y"] == des["pos_y"] and src["pos_x"] == des["pos_x"] and not src["pile"]:
+            return Outcome.INVALID
+
         return Outcome.VALID
 
     def _win_check(self, board) -> Outcome:
         '''
         Checks for a win
         '''
-        for y in range(0,5):
+        for y in range(0, 5):
 
             has_turned = False
-            for x in range(1,5):
+            for x in range(1, 5):
                 next_elem = self._find_top(board[x][y])
                 prev_elem = self._find_top(board[x - 1][y])
 
                 # Checks for valid turning paths
-                if prev_elem != -1 and next_elem != prev_elem and has_turned == False:
+                if prev_elem != -1 and next_elem != prev_elem and not has_turned:
                     has_turned = True
                     path_check = Outcome.CONT
 
                     if y > 0:
                         prev_down = self._find_top(board[x-1][y-1])
-                        if prev_elem == prev_down: path_check = self._check_path(x, y-1, board)
-                            
+                        if prev_elem == prev_down:
+                            path_check = self._check_path(x, y-1, board)
+
                     if y < 4:
                         prev_top = self._find_top(board[x-1][y+1])
-                        if prev_elem == prev_top: path_check = self._check_path(x, y+1, board)
+                        if prev_elem == prev_top:
+                            path_check = self._check_path(x, y+1, board)
 
                     if path_check != Outcome.CONT:
                         return path_check
-                    
+
                 if next_elem != prev_elem or prev_elem == -1:
                     break
             else:
@@ -65,8 +134,6 @@ class Validator:
         if arr[0] == 0:
             return -1
         for i, elem in enumerate(arr):
-            if elem == 0: 
+            if elem == 0:
                 return arr[i-1]
         return -1
-
-
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -5,12 +5,348 @@ from src.GameEngine.Objects.Outcome import Outcome
 from src.GameEngine.Objects.Enums import Orientation, Color
 v = Validator()
 
+
 # Init
 def test_init():
-    oldstate = { "board": np.zeros(shape=(5,5,42), dtype=object) }
-    newstate = { "board": np.zeros(shape=(5,5,42), dtype=object) }
-    result = v.check(oldstate, newstate)
-    assert(result == Outcome.VALID)
+    state = {"white_pieces_pile": 21, "black_pieces_pile": 21,
+             "board": np.zeros(shape=(5, 5, 42), dtype=object)}
+    move = {
+        "src": {
+            "pile": True,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 1,
+            "pos_y": 1,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 1,
+        "color": Color.WHITE,
+        "first_turn": False
+    }
+    result = v.check(move, state, state)
+    assert (result == Outcome.VALID)
+
+
+def test_out_of_bounds():
+    state = {"white_pieces_pile": 21, "black_pieces_pile": 21,
+             "board": np.zeros(shape=(5, 5, 42), dtype=object)}
+    move1 = {
+        "src": {
+            "pile": False,
+            "pos_x": 0,
+            "pos_y": 0,
+        },
+        "des": {
+            "pos_x": 1,
+            "pos_y": 1,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 1,
+        "color": Color.WHITE,
+        "first_turn": False
+    }
+    result = v.check(move1, state, state)
+    assert (result == Outcome.INVALID)
+
+    move2 = {
+        "src": {
+            "pile": False,
+            "pos_x": 6,
+            "pos_y": 5,
+        },
+        "des": {
+            "pos_x": 1,
+            "pos_y": 1,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 1,
+        "color": Color.WHITE,
+        "first_turn": False
+    }
+    result = v.check(move2, state, state)
+    assert (result == Outcome.INVALID)
+
+    move3 = {
+        "src": {
+            "pile": False,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 6,
+            "pos_y": 6,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 1,
+        "color": Color.WHITE,
+        "first_turn": False
+    }
+    result = v.check(move3, state, state)
+    assert (result == Outcome.INVALID)
+
+    move4 = {
+        "src": {
+            "pile": False,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 0,
+            "pos_y": 0,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 1,
+        "color": Color.WHITE,
+        "first_turn": False
+    }
+    result = v.check(move4, state, state)
+    assert (result == Outcome.INVALID)
+
+
+def test_invalid_n_pieces():
+    state = {"white_pieces_pile": 21, "black_pieces_pile": 21,
+             "board": np.zeros(shape=(5, 5, 42), dtype=object)}
+    move = {
+        "src": {
+            "pile": True,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 1,
+            "pos_y": 1,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 0,
+        "color": Color.WHITE,
+        "first_turn": False
+    }
+    result = v.check(move, state, state)
+    assert (result == Outcome.INVALID)
+
+
+def test_first_turn():
+    state = {"white_pieces_pile": 21, "black_pieces_pile": 21,
+             "board": np.zeros(shape=(5, 5, 42), dtype=object)}
+    move1 = {
+        "src": {
+            "pile": True,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 1,
+            "pos_y": 1,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 1,
+        "color": Color.WHITE,
+        "first_turn": True
+    }
+    result = v.check(move1, state, state)
+    assert (result == Outcome.INVALID)
+    move2 = {
+        "src": {
+            "pile": False,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 1,
+            "pos_y": 1,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 1,
+        "color": Color.WHITE,
+        "first_turn": True
+    }
+    result = v.check(move1, state, state)
+    assert (result == Outcome.INVALID)
+    move3 = {
+        "src": {
+            "pile": True,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 1,
+            "pos_y": 1,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 2,
+        "color": Color.WHITE,
+        "first_turn": True
+    }
+    result = v.check(move1, state, state)
+    assert (result == Outcome.INVALID)
+
+
+def test_place_on_wrong_color():
+    state = {"white_pieces_pile": 21, "black_pieces_pile": 21,
+             "board": np.zeros(shape=(5, 5, 42), dtype=object)}
+    state["board"][1][1][0] = Piece(Orientation.FLAT, Color.BLACK)
+    move1 = {
+        "src": {
+            "pile": True,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 1,
+            "pos_y": 1,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 1,
+        "color": Color.WHITE,
+        "first_turn": False
+    }
+    result = v.check(move1, state, state)
+    assert (result == Outcome.INVALID)
+
+
+def test_place_on_right_color():
+    state = {"white_pieces_pile": 21, "black_pieces_pile": 21,
+             "board": np.zeros(shape=(5, 5, 42), dtype=object)}
+    state["board"][1][1][0] = Piece(Orientation.FLAT, Color.BLACK)
+    move1 = {
+        "src": {
+            "pile": True,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 1,
+            "pos_y": 1,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 1,
+        "color": Color.BLACK,
+        "first_turn": False
+    }
+    result = v.check(move1, state, state)
+    assert (result == Outcome.VALID)
+
+
+def test_place_on_standing():
+    state = {"white_pieces_pile": 21, "black_pieces_pile": 21,
+             "board": np.zeros(shape=(5, 5, 42), dtype=object)}
+    state["board"][1][1][0] = Piece(Orientation.STANDING, Color.BLACK)
+    move1 = {
+        "src": {
+            "pile": True,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 1,
+            "pos_y": 1,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 1,
+        "color": Color.BLACK,
+        "first_turn": False
+    }
+    result = v.check(move1, state, state)
+    assert (result == Outcome.INVALID)
+
+
+def test_too_many_pieces():
+    state = {"white_pieces_pile": 21, "black_pieces_pile": 1,
+             "board": np.zeros(shape=(5, 5, 42), dtype=object)}
+    state["board"][1][1][0] = Piece(Orientation.FLAT, Color.BLACK)
+    move1 = {
+        "src": {
+            "pile": True,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 1,
+            "pos_y": 1,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 2,
+        "color": Color.BLACK,
+        "first_turn": False
+    }
+    result = v.check(move1, state, state)
+    assert (result == Outcome.INVALID)
+
+
+def test_move_stack():
+    state = {"white_pieces_pile": 21, "black_pieces_pile": 21,
+             "board": np.zeros(shape=(5, 5, 42), dtype=object)}
+    state["board"][1][1][0] = Piece(Orientation.FLAT, Color.BLACK)
+    state["board"][1][1][1] = Piece(Orientation.FLAT, Color.BLACK)
+    state["board"][1][1][2] = Piece(Orientation.FLAT, Color.BLACK)
+    move1 = {
+        "src": {
+            "pile": False,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 2,
+            "pos_y": 2,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 3,
+        "color": Color.BLACK,
+        "first_turn": False
+    }
+    result = v.check(move1, state, state)
+    assert (result == Outcome.VALID)
+
+
+def test_move_invalid_stack():
+    state = {"white_pieces_pile": 21, "black_pieces_pile": 21,
+             "board": np.zeros(shape=(5, 5, 42), dtype=object)}
+    state["board"][1][1][0] = Piece(Orientation.FLAT, Color.BLACK)
+    state["board"][1][1][1] = Piece(Orientation.FLAT, Color.WHITE)
+    state["board"][1][1][2] = Piece(Orientation.FLAT, Color.BLACK)
+    move1 = {
+        "src": {
+            "pile": False,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 2,
+            "pos_y": 2,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 3,
+        "color": Color.BLACK,
+        "first_turn": False
+    }
+    result = v.check(move1, state, state)
+    assert (result == Outcome.INVALID)
+
+
+def test_idle_move():
+    state = {"white_pieces_pile": 21, "black_pieces_pile": 21,
+             "board": np.zeros(shape=(5, 5, 42), dtype=object)}
+    state["board"][1][1][0] = Piece(Orientation.FLAT, Color.BLACK)
+    move1 = {
+        "src": {
+            "pile": False,
+            "pos_x": 1,
+            "pos_y": 1,
+        },
+        "des": {
+            "pos_x": 1,
+            "pos_y": 1,
+            "orientation": Orientation.FLAT
+        },
+        "pieces": 1,
+        "color": Color.BLACK,
+        "first_turn": False
+    }
+    result = v.check(move1, state, state)
+    assert (result == Outcome.INVALID)
+
 
 def test_find():
     arr = np.zeros(5, dtype=object)
@@ -23,23 +359,27 @@ def test_find():
     assert v._find_top(arr) == Piece(Orientation.FLAT, Color.WHITE)
 
 # Tests different win conditions
+
+
 def win_straight(color, orientation, outcome):
     boards = {}
-    for n in range(0,5):
-        boards[n] = np.zeros(shape=(5,5,42), dtype=object) 
-        for x in range(0,5):
+    for n in range(0, 5):
+        boards[n] = np.zeros(shape=(5, 5, 42), dtype=object)
+        for x in range(0, 5):
             boards[n][x][n][0] = Piece(orientation, color)
         result = v._win_check(boards[n])
-        assert( result == outcome )
+        assert (result == outcome)
 
 # Tests turns
+
+
 def cont_turns(color, orientation):
     boards = []
 
-    board0 = np.zeros(shape=(5,5,42), dtype=object) 
+    board0 = np.zeros(shape=(5, 5, 42), dtype=object)
     boards.append(board0)
 
-    board1 = np.zeros(shape=(5,5,42), dtype=object) 
+    board1 = np.zeros(shape=(5, 5, 42), dtype=object)
     board1[0][0][0] = Piece(orientation, color)
     board1[1][0][0] = Piece(orientation, color)
     board1[2][0][0] = Piece(orientation, color)
@@ -49,7 +389,7 @@ def cont_turns(color, orientation):
     board1[4][2][0] = Piece(orientation, color)
     boards.append(board1)
 
-    board2 = np.zeros(shape=(5,5,42), dtype=object) 
+    board2 = np.zeros(shape=(5, 5, 42), dtype=object)
     board2[0][0][0] = Piece(orientation, color)
     board2[1][0][0] = Piece(orientation, color)
     board2[2][0][0] = Piece(orientation, color)
@@ -59,7 +399,7 @@ def cont_turns(color, orientation):
     board2[4][2][0] = Piece(orientation, color)
     boards.append(board2)
 
-    board3 = np.zeros(shape=(5,5,42), dtype=object) 
+    board3 = np.zeros(shape=(5, 5, 42), dtype=object)
     board3[0][3][0] = Piece(orientation, color)
     board3[1][3][0] = Piece(orientation, color)
     board3[2][2][0] = Piece(orientation, color)
@@ -71,12 +411,13 @@ def cont_turns(color, orientation):
 
     for board in boards:
         result = v._win_check(board)
-        assert( result == Outcome.CONT )
+        assert (result == Outcome.CONT)
+
 
 def win_turns(color, orientation, outcome):
     boards = []
 
-    board1 = np.zeros(shape=(5,5,42), dtype=object) 
+    board1 = np.zeros(shape=(5, 5, 42), dtype=object)
     board1[0][0][0] = Piece(orientation, color)
     board1[1][0][0] = Piece(orientation, color)
     board1[1][1][0] = Piece(orientation, color)
@@ -85,7 +426,7 @@ def win_turns(color, orientation, outcome):
     board1[4][1][0] = Piece(orientation, color)
     boards.append(board1)
 
-    board2 = np.zeros(shape=(5,5,42), dtype=object) 
+    board2 = np.zeros(shape=(5, 5, 42), dtype=object)
     board2[0][4][0] = Piece(orientation, color)
     board2[1][4][0] = Piece(orientation, color)
     board2[1][3][0] = Piece(orientation, color)
@@ -94,7 +435,7 @@ def win_turns(color, orientation, outcome):
     board2[4][3][0] = Piece(orientation, color)
     boards.append(board2)
 
-    board3 = np.zeros(shape=(5,5,42), dtype=object) 
+    board3 = np.zeros(shape=(5, 5, 42), dtype=object)
     board3[0][3][0] = Piece(orientation, color)
     board3[1][3][0] = Piece(orientation, color)
     board3[2][4][0] = Piece(orientation, color)
@@ -105,48 +446,61 @@ def win_turns(color, orientation, outcome):
 
     for board in boards:
         result = v._win_check(board)
-        assert( result == outcome )
+        assert (result == outcome)
+
 
 def test_win_straight_white_standing():
     win_straight(Color.WHITE, Orientation.STANDING, Outcome.WIN_WHITE)
 
+
 def test_win_straight_white_flat():
     win_straight(Color.WHITE, Orientation.FLAT, Outcome.WIN_WHITE)
+
 
 def test_win_straight_black_standing():
     win_straight(Color.BLACK, Orientation.STANDING, Outcome.WIN_BLACK)
 
+
 def test_win_straight_black_flat():
     win_straight(Color.BLACK, Orientation.FLAT, Outcome.WIN_BLACK)
+
 
 def test_win_turns_white_standing():
     win_turns(Color.WHITE, Orientation.STANDING, Outcome.WIN_WHITE)
 
+
 def test_win_turns_white_flat():
     win_turns(Color.WHITE, Orientation.FLAT, Outcome.WIN_WHITE)
+
 
 def test_win_turns_black_standing():
     win_turns(Color.BLACK, Orientation.STANDING, Outcome.WIN_BLACK)
 
+
 def test_win_turns_black_flat():
     win_turns(Color.BLACK, Orientation.FLAT, Outcome.WIN_BLACK)
+
 
 def test_cont_turns_black_flat():
     cont_turns(Color.BLACK, Orientation.FLAT)
 
+
 def test_cont_turns_black_standing():
     cont_turns(Color.BLACK, Orientation.STANDING)
+
 
 def test_cont_turns_white_standing():
     cont_turns(Color.WHITE, Orientation.STANDING)
 
+
 def test_cont_turns_white_flat():
     cont_turns(Color.WHITE, Orientation.FLAT)
+
 
 def test_cont_mixed_1():
     outcome = Outcome.CONT
 
-    board = np.zeros(shape=(5,5,42), dtype=object) 
+    board = np.zeros(shape=(5, 5, 42), dtype=object)
     board[0][0][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[1][0][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[1][0][1] = Piece(Orientation.STANDING, Color.BLACK)
@@ -155,12 +509,13 @@ def test_cont_mixed_1():
     board[4][0][0] = Piece(Orientation.FLAT, Color.BLACK)
 
     result = v._win_check(board)
-    assert( result == outcome )
+    assert (result == outcome)
+
 
 def test_cont_mixed_2():
     outcome = Outcome.CONT
 
-    board = np.zeros(shape=(5,5,42), dtype=object) 
+    board = np.zeros(shape=(5, 5, 42), dtype=object)
     board[0][0][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[1][0][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[1][0][1] = Piece(Orientation.FLAT, Color.WHITE)
@@ -169,11 +524,12 @@ def test_cont_mixed_2():
     board[4][0][0] = Piece(Orientation.FLAT, Color.BLACK)
 
     result = v._win_check(board)
-    assert( result == outcome )
+    assert (result == outcome)
+
 
 def test_cont_mixed_3():
     outcome = Outcome.CONT
-    board = np.zeros(shape=(5,5,42), dtype=object) 
+    board = np.zeros(shape=(5, 5, 42), dtype=object)
     board[0][0][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[1][0][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[1][0][1] = Piece(Orientation.STANDING, Color.WHITE)
@@ -181,23 +537,25 @@ def test_cont_mixed_3():
     board[3][0][0] = Piece(Orientation.STANDING, Color.BLACK)
     board[4][0][0] = Piece(Orientation.FLAT, Color.BLACK)
     result = v._win_check(board)
-    assert( result == outcome )
-        
+    assert (result == outcome)
+
+
 def test_cont_diagonal_1():
     outcome = Outcome.CONT
 
-    board = np.zeros(shape=(5,5,42), dtype=object) 
+    board = np.zeros(shape=(5, 5, 42), dtype=object)
     board[0][0][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[1][1][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[2][2][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[3][3][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[4][4][0] = Piece(Orientation.FLAT, Color.BLACK)
     result = v._win_check(board)
-    assert( result == outcome )
+    assert (result == outcome)
+
 
 def test_win_mixed_1():
     outcome = Outcome.WIN_BLACK
-    board = np.zeros(shape=(5,5,42), dtype=object) 
+    board = np.zeros(shape=(5, 5, 42), dtype=object)
 
     board[0][4][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[1][4][0] = Piece(Orientation.FLAT, Color.BLACK)
@@ -209,11 +567,12 @@ def test_win_mixed_1():
     board[4][3][0] = Piece(Orientation.FLAT, Color.BLACK)
 
     result = v._win_check(board)
-    assert( result == outcome )
+    assert (result == outcome)
+
 
 def test_win_mixed_2():
     outcome = Outcome.WIN_BLACK
-    board = np.zeros(shape=(5,5,42), dtype=object) 
+    board = np.zeros(shape=(5, 5, 42), dtype=object)
 
     board[0][4][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[1][4][0] = Piece(Orientation.STANDING, Color.BLACK)
@@ -226,11 +585,12 @@ def test_win_mixed_2():
     board[4][3][0] = Piece(Orientation.FLAT, Color.BLACK)
 
     result = v._win_check(board)
-    assert( result == outcome )
+    assert (result == outcome)
+
 
 def test_win_multiple_turns_1():
     outcome = Outcome.WIN_BLACK
-    board = np.zeros(shape=(5,5,42), dtype=object) 
+    board = np.zeros(shape=(5, 5, 42), dtype=object)
 
     board[0][2][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[1][2][0] = Piece(Orientation.FLAT, Color.BLACK)
@@ -243,11 +603,12 @@ def test_win_multiple_turns_1():
     board[4][1][0] = Piece(Orientation.FLAT, Color.BLACK)
 
     result = v._win_check(board)
-    assert( result == outcome )
+    assert (result == outcome)
+
 
 def test_win_multiple_turns_2():
     outcome = Outcome.WIN_BLACK
-    board = np.zeros(shape=(5,5,42), dtype=object) 
+    board = np.zeros(shape=(5, 5, 42), dtype=object)
 
     board[0][2][0] = Piece(Orientation.FLAT, Color.BLACK)
     board[1][2][0] = Piece(Orientation.FLAT, Color.BLACK)
@@ -260,4 +621,4 @@ def test_win_multiple_turns_2():
     board[3][1][0] = Piece(Orientation.FLAT, Color.BLACK)
 
     result = v._win_check(board)
-    assert( result == outcome )
+    assert (result == outcome)
